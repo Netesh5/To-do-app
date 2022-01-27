10da6a4eeaf47bbec319c13d034b13b556bd3075
netesh5 <netesh.paudel@gmail.com>
2022-01-27 13:19:53 +0545

    file creation

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void runApp() {
+  myApp();
+}
+
+class myApp extends StatefulWidget {
+  myApp({Key? key}) : super(key: key);
+
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
